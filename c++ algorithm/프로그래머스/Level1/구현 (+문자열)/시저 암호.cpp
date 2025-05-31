8b4ea56c0b8ef8e7fa34411b5108bc91df9a1f79
lch_98 <dlcndgusgnss@naver.com>
2025-06-01 00:30:41 +0900

    code Refactoring LV1 programmers

--- c++ algorithm/프로그래머스/Level1/구현 (+문자열)/시저 암호.cpp
+++ c++ algorithm/프로그래머스/Level1/구현 (+문자열)/시저 암호.cpp
@@ -5,25 +5,30 @@ using namespace std;
 
 string solution(string s, int n) {
     string answer = "";
-
-    for (int i = 0; i < s.size(); i++) {
-        char c = s[i];
-
-        // 공백은 그대로 추가
-        if (c == ' ') {
-            answer += ' ';
+    
+    for (int i=0; i<s.size(); i++) {
+        // 공백이면 패스
+        if (s[i] == ' ') continue;
+        
+        // 소문자일때
+        else if (s[i] >= 'a' && s[i] <= 'z') {
+            int test = s[i] + n;
+            if (test > 122) {
+                test = test - 26; // 'z' = 122, 'a' = 96
+            }
+            s[i] = char(test);
         }
-        // 소문자
-        else if (c >= 'a' && c <= 'z') {
-            c = (c - 'a' + n) % 26 + 'a';
-            answer += c;
-        }
-        // 대문자
-        else if (c >= 'A' && c <= 'Z') {
-            c = (c - 'A' + n) % 26 + 'A';
-            answer += c;
+        
+        // 대문자일때
+        else if (s[i] >= 'A' && s[i] <= 'Z') {
+            int test = s[i] + n;
+            if (test > 90) {
+                test = test - 26; // 'z' = 90, 'a' = 64
+            }
+            s[i] = char(test); 
         }
     }
-
+    
+    answer = s;
     return answer;
 }
