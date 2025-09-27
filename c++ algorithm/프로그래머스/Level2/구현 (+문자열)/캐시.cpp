acbc428b3de7a846bb1207dde60aaadf606b7c6b
lch_98 <dlcndgusgnss@naver.com>
2025-09-27 12:17:20 +0900

    Programmers LV2

--- c++ algorithm/프로그래머스/Level2/구현 (+문자열)/캐시.cpp
+++ c++ algorithm/프로그래머스/Level2/구현 (+문자열)/캐시.cpp
@@ -3,6 +3,16 @@
 #include <algorithm>
 using namespace std;
 
+string lower(string str) {
+    for (int i=0; i<str.size(); i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') continue;
+        else if (str[i] >= 'A' && str[i] <= 'Z') {
+            str[i] += ('a' - 'A');
+        }
+    }
+    return str;
+}
+
 int solution(int cacheSize, vector<string> cities) {
     int answer = 0;
     vector<string> cache;
@@ -10,19 +20,19 @@ int solution(int cacheSize, vector<string> cities) {
     if (cacheSize == 0) return cities.size() * 5;
 
     for (string city : cities) {
-        transform(city.begin(), city.end(), city.begin(), ::tolower);
+        string transform_city = lower(city);
 
-        auto it = find(cache.begin(), cache.end(), city);
+        auto it = find(cache.begin(), cache.end(), transform_city);
 
         if (it == cache.end()) { // cache miss
             if (cache.size() == cacheSize) {
                 cache.erase(cache.begin()); // 가장 오래된 것 제거
             }
-            cache.push_back(city);
+            cache.push_back(transform_city);
             answer += 5;
         } else { // cache hit
             cache.erase(it);          // 기존 위치에서 제거
-            cache.push_back(city);    // 맨 뒤에 다시 삽입 (최근 사용)
+            cache.push_back(transform_city);    // 맨 뒤에 다시 삽입 (최근 사용)
             answer += 1;
         }
     }
